e4e678c66acd4bd5cc6bc747d16adf4b9f56a9b3
Anderson Viana <andersonp.viana.contato@gmail.com>
2023-05-10 09:48:10 -0300

    Configurando o database e o sequelize

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost', 
+  username:,
+  password:,
+  database: 'tasklist',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+}
