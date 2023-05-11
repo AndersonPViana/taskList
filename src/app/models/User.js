d6dd31069be0732860043aaf9b9a51cc1c0cdd0d
Anderson Viana <andersonp.viana.contato@gmail.com>
2023-05-11 18:27:03 -0300

    Criptografando password

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize');
 const { Model } = require('sequelize');
+const bcrypt = require('bcrypt')
 
 class User extends Model {
   static init(sequelize) {
@@ -7,12 +8,17 @@ class User extends Model {
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING
       },
       {
         sequelize,
       }
     );
+
+    this.addHook('beforeSave', async (user) => {
+      user.password_hash = await bcrypt.hash(user.password, 8);
+    });
   }
 }
 
