cf0b05a4f3ebc2ff9cf4abf6902661af2b000b9d
Anderson Viana <andersonp.viana.contato@gmail.com>
2023-05-11 16:40:57 -0300

    Criando UserController e fazendo um store básico

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,11 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    const {name, email, password_hash} = req.body
+
+    const user = await User.create(name, email, password_hash);
+
+    return res.status(200).json({ message: 'User created' })
+  }
+}
