fd6c90f79cf5e5dd0043998c433ef82737127462
Anderson Viana <andersonp.viana.contato@gmail.com>
2023-05-12 10:07:40 -0300

    Criando o token

--- src/app/controllers/SessionsController.js
+++ src/app/controllers/SessionsController.js
@@ -7,8 +7,25 @@ class SessionsController {
 
     const user = await User.findOne({where: {email}})
     if(!user) {
-      return res.status(401).json('User does not exist')
+      return res.status(401).json( { error: 'User does not exist' })
     }
 
+    if(!(await user.checkpassword(password))) {
+      return res.status(401).json({ error: 'Incorrect password' })
+    }
+
+    const { id, name } = user;
+
+    return res.json({
+      user: {
+        id, 
+        name,
+        email
+      },
+      token: jwt.sign({ id }, '99415723b33478223233245607d50ac3', 
+      {expiresIn: '7d'}),
+    });
   }
 }
+
+module.exports = new SessionsController();
