89882282f5a4f020899457ebf89bf95c8265d593
Anderson Viana <andersonp.viana.contato@gmail.com>
2023-05-15 17:02:07 -0300

    Acrescentando UPDATE em usuário

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -20,7 +20,31 @@ class UserController {
   }
 
   async update(req, res) {
+    const { email, oldPassword } = req.body;
     
+    const user = await User.findByPk(req.userId);
+
+    if(email !== user.email) {
+      const userExist = await User.findOne({
+        where: {email},
+      });
+
+      if(userExist) {
+        return res.status(400).json({ error: 'User already exists' });
+      }
+    }
+
+    if(oldPassword && !(await user.checkpassword(oldPassword))){
+      return res.status(401).json({ error: 'incorrect password' });
+    }
+
+    const { id, name } = await User.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email
+    })
   }
 }
 
