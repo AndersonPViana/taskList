853055b73d251872fa24b0516a52ab4eeb220312
Anderson Viana <andersonp.viana.contato@gmail.com>
2023-05-16 09:28:47 -0300

    Carregando o model Task no database

--- src/database/index.js
+++ src/database/index.js
@@ -2,8 +2,9 @@ const Sequelize = require('sequelize');
 const databaseConfig = require('../config/database');
 
 const User = require('../app/models/User');
+const Task = require('../app/models/Task');
 
-const models = [User];
+const models = [User, Task];
 
 class Database {
   constructor() {
@@ -13,8 +14,8 @@ class Database {
   init() {
     this.connection = new Sequelize(databaseConfig);
 
-    models.map(model => model.init(this.connection));
-
+    models.map(model => model.init(this.connection))
+          .map(model => model.associate && model.associate(this.connection.models));
   }
 }
 
