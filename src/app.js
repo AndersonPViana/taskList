1c5651bbd1c9fb89d0d016e1314d13ee78b90a58
Anderson Viana <andersonp.viana.contato@gmail.com>
2023-05-20 10:33:16 -0300

    Acrescentando o cors no projeto

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors')
 
 const routes = require('./app/routes/routes.js');
 
@@ -6,9 +7,11 @@ require('./database');
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
 
-app.use(routes)
+app.use(routes);
 
 app.listen(3000, () => {
   console.log('Server on !')
