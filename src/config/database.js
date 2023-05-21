4113eae1415e0b554964bd2306e077e9d178799a
Anderson Viana <andersonp.viana.contato@gmail.com>
2023-05-21 14:41:53 -0300

    Acrescentando o Dotenv para esconder a senha do database

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,10 @@
+require('dotenv').config;
+
 module.exports = {
   dialect: 'postgres',
   host: 'localhost', 
   username: 'postgres',
-  password: '',
+  password: process.env.CONNECTIONSTRING,
   database: 'tasklist',
   define: {
     timestamps: true,
